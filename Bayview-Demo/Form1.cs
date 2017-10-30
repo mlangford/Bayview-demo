df5b1dd9c0b55a44041e600b079421a335d15ba5
Mitch <mitchel.langford@southwales.ac.uk>
2017-10-30 15:44:27 +0000

    Added basic login sequence

--- Bayview-Demo/Form1.cs
+++ Bayview-Demo/Form1.cs
@@ -11,10 +11,39 @@ namespace Bayview_Demo
             InitializeComponent();
         }
 
+        //global variable used to keep track of
+        //the currently logged-in staff member
+        int stfid;
+
         private void btnlogin_Click(object sender, EventArgs e)
         {
             try
             {
+                using (SQLiteConnection dbcon = new SQLiteConnection())
+                {
+                    dbcon.ConnectionString = dbConnection.source;
+                    //we wish to retrieve the password and staff-ID
+                    string sql = "SELECT passwd, staffID FROM staff WHERE staffname=@name";
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, dbcon))
+                    {
+                        //using a user-supplied staff name
+                        cmd.Parameters.AddWithValue("name", tbusername.Text);
+                        dbcon.Open();
+                        using (SQLiteDataReader dr = cmd.ExecuteReader())
+                        {
+                            //if no match to name in Db, throw an exception
+                            if (!dr.HasRows)
+                                throw new Exception();
+                            //if paswords dont match, throw an exception
+                            dr.Read();
+                            if (tbpasswd.Text != dr[0].ToString())
+                                throw new Exception();
+                            //OK, the login is valid
+                            stfid = Convert.ToInt32(dr[1]);  //keep track of current staff member
+                            dbcon.Close();
+                        }
+                    }
+                }
             }
             catch
             {
